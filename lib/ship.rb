1e901b581fdf4516e931a5470004c556a22e232d
Carol Rieping <carol.rieping@gmail.com>
2022-12-06 07:41:33 -0700

    Add lib and spec files

---
+++ lib/ship.rb
@@ -0,0 +1,11 @@
+class Ship
+    
+    attr :name,
+         :length
+    
+    def initialize(name, length)
+        @name = name
+        @length = length
+    end
+
+end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,12 +1,13 @@
 require './lib/ship'
 
 describe Ship do
+
     it 'exists' do
         cruiser = Ship.new("Cruiser", 3)
-        
-        expect(cruiser.name).to be_a("Cruiser")
-        expect(cruiser.length).to eq(3)
+
+        expect(ship).to be_a(Ship)
     end
+
 
 
 end
