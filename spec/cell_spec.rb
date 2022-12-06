3f0b7761b76cd8b55a0f439493346b7b922a1c50
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-12-06 12:31:43 -0700

    Add empty method & place_ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -6,12 +6,23 @@ describe Cell do
         cell = Cell.new("B4")
 
         expect(cell).to be_a(Cell)
+    end
 
-    it "is empty?" do
+    it "is the cell empty?" do
         cell = Cell.new("B4")
 
         expect(cell.coordinate).to eq("B4")
         expect(cell.ship).to eq(nil)
-        cell.empty?.to eq(true)
+        expect(cell.empty?).to eq(true)
+    end
+
+    it "place_ship" do
+        cell = Cell.new("B4")
+        cruiser = Ship.new("Cruiser", 3)
+
+        cell.place_ship(cruiser)
+        
+        expect(cell.ship).to eq("Cruiser")
+        expect(cell.empty?).to eq(false)
     end
 end
